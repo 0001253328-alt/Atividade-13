a38c76fb87ec95b96eee31168fcc0545e04fdd30
Sabylie <0001253328@senaimgaluno.com.br>
2026-06-21 10:51:26 -0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,5 @@
+
+let n = Number (prompt("Digite um numero: "))
+for(let i=1; i<=10; i++) {
+    alert (`${n} x ${i} = ${n*i}`)
+}
---
+++ 2.js
@@ -0,0 +1,6 @@
+let N = Number(prompt("Digite um numero limite"))
+let soma = 0
+for (let i=1;i<=N; i++){
+    soma = soma + i
+}
+console.log (`Qual a soma de todos os numeros até o número limte é: ${soma}`)
